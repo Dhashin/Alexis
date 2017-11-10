758dbd87ea2e1e6036749e6bb9984b36462022f2
dhashinmoodley@yahoo.com <dhashinmoodley@yahoo.com>
2017-11-10 12:56:45 +0000

    more and more LED programs

--- turnon.js
+++ turnon.js
@@ -2,4 +2,15 @@ var Gpio= require('onoff').Gpio,
 led =new Gpio(17, 'out');
 led2 =new Gpio(27, 'out');
 led.writeSync(1);
+wait(3000);
 led2.writeSync(1);
+
+function wait(ms)
+{
+var start = new Date().getTime();
+var end = start;
+while(end < start + ms)
+{
+end = new Date().getTime();
+}
+}
