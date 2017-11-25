d85cc5a179c42145714f16f47c97f457be11c168
dhashinmoodley@yahoo.com <dhashinmoodley@yahoo.com>
2017-11-25 23:16:37 +0000

    updates

--- xmas.js
+++ xmas.js
@@ -2,6 +2,8 @@ var Gpio = require('onoff').Gpio,
     led1 = new Gpio(17, 'out');
 led2 = new Gpio(27,'out');
 led3 = new Gpio(22, 'out');
+led4 = new Gpio(18, 'out');
+led5 = new Gpio(23, 'out');
 
 led1.writeSync(1);
 wait(3000);
@@ -14,18 +16,28 @@ wait(3000);
 led3.writeSync(0);
 
 
-for (i=0;i<10;i++)
+for (i=0;i<100;i++)
 {
+led4.writeSync(1);
+led5.writeSync(1);
 led1.writeSync(1);
 wait(100);
+led4.writeSync(0);
+led5.writeSync(0);
 led1.writeSync(0);
 led2.writeSync(1);
 wait(100);
 led2.writeSync(0);
 led3.writeSync(1);
+led4.writeSync(1);
+led5.writeSync(1);
 wait(100);
 led3.writeSync(0);
+
+led4.writeSync(0);
+led5.writeSync(0);
 }
+
 function wait(ms){
    var start = new Date().getTime();
    var end = start;
